0c91c0789bc7288fad5d16f53c45f5a5ebda1aff
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-09-29 12:02:05 +0200

    responsive navbar

---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,40 @@
+import { NavData } from '../../data/NavData';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../../assets/logo.png';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="" variant="dark" expand="lg" collapseOnSelect className="navbar-fixed-top">
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">
+              <img
+                src={logo}
+                alt="logo"
+                style={{ maxHeight: '100px', marginTop: '-10px' }}
+                className="hidden-xs"
+              />
+            </Link>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              {NavData.map((item, i) => (
+                <Link to={item.link} key={i} className="nav-menu-links">
+                  {item.title}
+                </Link>
+              ))}
+              <br />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/data/NavData.tsx
@@ -0,0 +1,28 @@
+export const NavData = [
+  { title: 'Welcome', link: '/' },
+
+  {
+    title: 'About',
+    link: '/about',
+  },
+  {
+    title: 'Schedules',
+    link: '/schedules',
+  },
+  {
+    title: 'Gallery',
+    link: '/gallery',
+  },
+  {
+    title: 'Sponsors',
+    link: '/sponsors',
+  },
+  {
+    title: 'Contact ',
+    link: '/contact',
+  },
+  {
+    title: 'Sign In ',
+    link: '/register',
+  },
+];
