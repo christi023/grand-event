928c6161517f44d00adba388680de133f7a1d520
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-10-03 13:53:49 +0200

    Holds the Carrousel, Event Timer component

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,17 +1,21 @@
 import { Link } from 'react-router-dom';
 import Carrousel from '../Carrousel/Carrousel';
 import Banner from '../Banner/Banner';
+import EventTimer from '../EventTimer/EventTimer';
 
 const Home = () => {
   return (
     <>
-      <Carrousel hero={'defaultHero'} hero1={'defaultHero1'} hero2={'defaultHero2'}>
-        <Banner title="Grand Events" subtitle="Tickets starting at $275">
-          <Link to="/tickets" className="btn btn-primary">
-            Buy Ticket
-          </Link>
-        </Banner>
-      </Carrousel>
+      <div style={{ position: 'relative' }}>
+        <Carrousel hero={'defaultHero'} hero1={'defaultHero1'} hero2={'defaultHero2'}>
+          <Banner title="Grand Events" subtitle="Tickets starting at $275">
+            <Link to="/tickets" className="btn btn-primary">
+              Buy Ticket
+            </Link>
+          </Banner>
+        </Carrousel>
+      </div>
+      <EventTimer />
     </>
   );
 };
